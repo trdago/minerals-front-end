bd54c89bd4967eec09c847d4c2da19363605f51f
dcarrasco <dcarrascocid@gmail.com>
2023-05-05 00:21:11 -0400

    servicios de crear cotizacion mofificado

--- src/store/modules/cotizaciones.js
+++ src/store/modules/cotizaciones.js
@@ -538,7 +538,7 @@ const actions = {
         let loading = payload.loading.show()
 
         try {
-            const { data } =  await axios.post('/api/quotations/accion', payload)
+            const { data } =  await axios.post('/api/quotations/new', payload)
 
             console.log(data)
 
